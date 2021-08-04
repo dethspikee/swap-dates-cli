1b54e440579f559a6295403082ed9028be66772d
Damian Tabor <zettie008@gmail.com>
2021-08-04 21:20:34 +0100

    initial commit

---
+++ fixdates.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Incorrect number of arguments.\nusage: fixdates csvfile\n");
+        return EXIT_FAILURE;
+    }
+
+    char buffer[512];
+    FILE *ptr;
+
+    if ((ptr = open(argv[1]) == NULL)) {
+        fprintf(stderr, "Cannot open the file: %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+}
