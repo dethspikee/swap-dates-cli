d29a2b7ebc87b960bf1f279daac41bd5a179c616
Damian Tabor <zettie008@gmail.com>
2021-08-04 22:35:24 +0100

    construct new date

--- fixdates.c
+++ fixdates.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+
+
+char *swap_date(char *date);
 
 
 int main(int argc, char *argv[]) {
@@ -9,10 +13,12 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    char buffer[512];
-    char *ptr_to_str;
     FILE *ptr;
+    char buffer[512];
+    char *ptr_to_date;
+    int column = 0;
     int counter = 0;
+    bool headers_read = false;
 
     if ((ptr = fopen(argv[1], "r")) == NULL) {
         perror("Cannot open input file.\n");
@@ -20,17 +26,42 @@ int main(int argc, char *argv[]) {
     }
 
     while ((fgets(buffer, sizeof(buffer), ptr) != NULL)) {
-        ptr_to_str = strtok(buffer, ",");
-        counter++;
-        while ((ptr_to_str = strtok(NULL, ",")) != NULL) {
-            counter++;
-            if (counter == 3) {
-                printf("%s\n", ptr_to_str);
-                counter = 0;
+        if (!headers_read) {
+            headers_read = true;
+            continue;
+        }
+
+        ptr_to_date = strtok(buffer, ",");
+        column++;
+        while ((ptr_to_date = strtok(NULL, ",")) != NULL) {
+            column++;
+            if (column == 3) {
+                swap_date(ptr_to_date);
+                column = 0;
+                counter++;
+                break;
+            }
+            if (counter == 50) {
                 break;
             }
         }
     }
 
     return EXIT_SUCCESS;
+}
+
+
+char *swap_date(char *date) {
+    char buffer[32] = {0};
+    char *month = strtok(date, "/");
+    char *day = strtok(NULL, "/");
+    char *year = strtok(NULL, "/");
+
+    strncpy(buffer, day, strlen(month));
+    strcat(buffer, "/");
+    strcat(buffer, month);
+    strcat(buffer, "/");
+    strcat(buffer, year);
+
+    printf("day: %s\n", buffer);
 }
